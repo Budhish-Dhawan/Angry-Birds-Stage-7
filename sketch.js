fd247dd3956e430e789489b1b56662a4e9498998
Budhish-Dhawan <budhishdhawan@gmail.com>
2021-01-10 15:57:00 +0530

    trajectory of the bird

--- sketch.js
+++ sketch.js
@@ -1,3 +1,67 @@
+/*
+
+
+//Examples on different types of data in JavaScript
+
+//Number
+var num =  89;
+console.log(num);
+
+//String
+var name = "Budhish";
+console.log(name);
+
+//Boolean - true or false
+var bool = true;
+console.log(bool);
+
+//Undefined
+var object;
+console.log(object);
+
+//Null
+//Reassigning the value of undefined object to null
+object = null;
+console.log(object);
+
+//array having values with same data type
+var arr1 = [23,45,67,89];
+console.log(arr1);
+
+//array having values with different data type
+var arr2 = [34.5, "hello", false, null, 56];
+console.log(arr2);
+
+//length of the array
+console.log(arr1.length);
+console.log(arr2.length);
+
+//accessing the elements of the array
+console.log(arr1[2]); //67
+console.log(arr1[arr1.length-1]);//89
+console.log(arr2[1]);
+
+//array inside array
+var arr3 = ["mango", "banana", false, [1,2,3,4,5], [6,7,8,9,10],[56,78,[34,23]]];
+console.log(arr3);
+console.log(arr3.length);
+console.log(arr3[0]); //mango
+console.log(arr3[3][3]);//4
+console.log(arr3[4][1]);//7
+console.log(arr3[5][2][1]);//23
+
+//pushing elements inside array
+var arr4 = ["apple", "mango", "kiwi", "guava"];
+console.log(arr4);
+arr4.push("orange");
+arr4.push("blueberry", "apricot", "papaya");
+console.log(arr4);
+
+//pop the element out of an array
+arr4.pop();
+console.log(arr4);
+*/
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,6 +71,8 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+
+var gameState = "onSling";
 
 
 function preload() {
@@ -69,12 +135,16 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState !== 'launched'){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+    
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
 function keyPressed(){
